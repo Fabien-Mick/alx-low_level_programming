800618ab1f3ff8f67fcf07dddeaf53da328913a1
Fabien-Mick <fabien.mick@gmail.com>
2022-09-10 09:59:32 +0000

    adjust the variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	char d;
 	float e;
 
-	printf("Size of char: %zu byte(s)\n", sizeof(a));
+	printf("Size of char: %zu byte(s)\n", sizeof(d));
+	printf("Size of an int: %zu byte(s)\n", sizeof(a));
 	printf("size of a long int: %zu byte(s)\n", sizeof(b));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(c));
-	printf("Size of a char: %zu byte(s)\n", sizeof(d));
 	printf("Size of a float: %zu byte(s)\n,", sizeof(e));
 	return (0);
 }
