cad9aba7f7dba777cfd6ff9bbffa170950f2eff0
Fabien-Mick <fabien.mick@gmail.com>
2022-09-10 11:14:52 +0000

    fixed the glich

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	char d;
 	float e;
 
-	printf("Size of char: %zu byte(s)\n", sizeof(d));
+	printf("Size of a char: %zu byte(s)\n", sizeof(d));
 	printf("Size of an int: %zu byte(s)\n", sizeof(a));
 	printf("size of a long int: %zu byte(s)\n", sizeof(b));
 	printf("size of a long long int: %zu byte(s)\n", sizeof(c));
