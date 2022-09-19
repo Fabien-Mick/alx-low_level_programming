755ebe3f11d4b4784915ed26d9261194784798a1
Fabien-Mick <fabien.mick@gmail.com>
2022-09-19 19:21:15 +0000

    use of strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen -  func returns length of string.
+ * @s: An input string
+ * Return: Nothing
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
