8127636c78b2e395f8710eca31b59e3c4f439289
Fabien-Mick <fabien.mick@gmail.com>
2022-09-19 21:06:24 +0000

    Fix again

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,6 @@
+#include "main.h"
 /**
- * _strcpy - func copies the string pointed to by src,
+ * _strcpy - function copies the string pointed to by src,
  * including the terminating null byte (\0),
  * to the buffer pointed to by dest.
  *
@@ -10,9 +11,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *aux = dest;
-
-	while (*src)
-		*dest++ = *src++;
-	return (aux);
+int inc = 0;
+while (*(src + inc) != '\0')
+{
+*(dest + inc) = *(src + inc);
+inc++;
+}
+*(dest + inc) = '\0';
+return (dest);
 }
