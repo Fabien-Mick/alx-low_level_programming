9d03460e4fd9a7c434abbf6748a6865f2623a977
Fabien-Mick <fabien.mick@gmail.com>
2022-12-10 12:27:15 +0000

    return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: 1
+  */
+
+int main(void)
+{
+	putchar("'a''n''d' 't''h''a''t' 'p''i''e''c''e' 'o''f' 'a''r''t' 'i''s' 'u''s''e''f''u''l''"' '-' 'D''o''r''a' 'K''o''r''p''a''r'',' '2''0''1''5''-''1''0''-''1''9'");
+	return -1;
+
+	return (0);
+}
