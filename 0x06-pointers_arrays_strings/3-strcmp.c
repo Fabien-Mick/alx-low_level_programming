39eb369fcce13ef4db423fe80d9f8e5ee044c594
Fabien-Mick <fabien.mick@gmail.com>
2022-12-22 15:31:31 +0000

    fix the copy

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,13 +16,15 @@ int _strcmp(char *s1, char *s2)
 	if (*s1 - *s2 > '0')
 	{
 		return (-i);
+		s1++,s2++;
 	}
 	else if (*s1 - *s2 < '0')
 	{
 		return (i);
+		s1++, s2++;
 	}
 	else
 	{
-		return ('0');
+		return ('i');
 	}
 }
