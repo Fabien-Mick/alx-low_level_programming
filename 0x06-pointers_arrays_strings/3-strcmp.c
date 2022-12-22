d430d209b270cfaf571d968dc63ea528174f447b
Fabien-Mick <fabien.mick@gmail.com>
2022-12-22 15:17:46 +0000

    compare 2 strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+  * _strcmp - compares 2 strings
+  * @s1: first string
+  * @s2: second string
+  * Return: *s1 - *s2
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = *s1 - *s2;
+
+	if (*s1 - *s2 > '0')
+	{
+		return (-i);
+	}
+	else if (*s1 - *s2 < '0')
+	{
+		return (i);
+	}
+	else
+	{
+		return ('0');
+	}
+}
