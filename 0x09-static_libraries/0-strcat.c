6b668ac6d2dcd433aef07692ebb2588103a1283b
Fabien-Mick <fabien.mick@gmail.com>
2023-01-06 17:36:03 +0000

    adding the last file

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
-   * _strcat - Concat two string @src and @dest, including the terminating
-    * null byte.
-     * @dest: A pointer to the string to be concatenated
-      * @src: A pointer to the string to append to @dest
-       *
-        * Return: A pointer to the resulting string dest
-	 */
+  * _strcat - Concat two string @src and @dest, including the terminating
+  * null byte.
+  * @dest: A pointer to the string to be concatenated
+  * @src: A pointer to the string to append to @dest
+  *
+  * Return: A pointer to the resulting string dest
+*/
 
 char *_strcat(char *dest, char *src)
 {
-		int index = 0, dest_len = 0;
+	int index = 0, dest_len = 0;
 
-			while (dest[index++])
-						dest_len++;
+	while (dest[index++])
+		dest_len++;
 
-				for (index = 0; src[index]; index++)
-							dest[dest_len++] = src[index];
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-					return (dest);
+	return (dest);
 }
