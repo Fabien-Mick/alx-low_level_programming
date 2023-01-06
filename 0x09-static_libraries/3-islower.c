a8f5a2d2f75ad57789e606303ae7096a7d1834a1
Fabien-Mick <fabien.mick@gmail.com>
2023-01-06 16:41:41 +0000

    Add all missing files

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+    * _islower - check for lower case character
+      * @c: An input characteri
+        * Return: 1 if lower or else 0
+	  */
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+				{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
