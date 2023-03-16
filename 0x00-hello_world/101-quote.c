e4d24ff7032d42213e77ca2e3ebefbb54ab83628
Fabien-Mick <fabien.mick@gmail.com>
2023-03-16 15:02:43 +0000

    Try a new approach

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,6 @@
 */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-int i;
-
-	for (i = 0; message[i] != '\0'; i++)
-	{
-	write(1, &message[i], 1);
-	}
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
