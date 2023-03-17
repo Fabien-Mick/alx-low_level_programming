8ea9c6935e4620bf6c1947748787570b85185e70
Fabien-Mick <fabien.mick@gmail.com>
2023-03-17 17:02:47 +0000

    Refine the code and add braces

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+  * main- Entry point
+  *
+  * Return: Always(0)
+  */
 
 int main(void)
 {
-	int d;
-	char low;
+	int num;
+	int alph;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-		putchar(low);
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (alph = 'a'; alph <= 'f'; alph++)
+	{
+		putchar(alph);
+	}
 	putchar('\n');
-
 	return (0);
 }
