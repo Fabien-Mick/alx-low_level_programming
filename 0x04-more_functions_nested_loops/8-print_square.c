30b5b993b98fda60b277814badc28bcb41f056c2
Fabien-Mick <fabien.mick@gmail.com>
2023-03-23 18:03:08 +0000

    Print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,21 +8,21 @@
  */
 void print_square(int size)
 {
-	int i = size;
-	int j = size;
+	int i, j;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar ('\n');
-		return;
-	}
-	while (i--)
-	{
-		while (j--)
+		for (i = 0; i < size; i++)
 		{
-			_putchar ('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar ('#');
+			}
+			_putchar ('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
-		j = size;
 	}
 }
