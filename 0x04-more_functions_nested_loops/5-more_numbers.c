d0aa82f54286b9f1cb35dfe626e06495189cb487
Fabien-Mick <fabien.mick@gmail.com>
2023-03-23 12:54:34 +0000

    Hone the code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,18 @@
  */
 void more_numbers(void)
 {
-	int i, j = 0;
+	int i, j;
 
-	for (; j < 10; j++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (i > 9)
-				_putchar(i / 10 + '0');
-
-			_putchar(i % 10 + '0');
+			if (j > 9)
+			{
+				_putchar ((j / 10) + '0');
+			}
+			_putchar ((j % 10) + '0');
 		}
-		_putchar('\n');
+		_putchar ('\n');
 	}
 }
