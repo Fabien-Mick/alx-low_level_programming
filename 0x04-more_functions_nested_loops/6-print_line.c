07924ca935d5978faf7de0628045f2aa416c1360
Fabien-Mick <fabien.mick@gmail.com>
2023-03-23 14:54:40 +0000

    Make alx happy

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,18 +8,12 @@
  */
 void print_line(int n)
 {
-	int i;
+	int i = 0;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar ('\n');
-
-		return;
+		for (; i < n; i++)
+			_putchar('_');
 	}
-
-	for (i = 0; i < n; i++)
-	{
-		_putchar ('-');
-	}
-	_putchar ('\n');
+	_putchar('\n');
 }
