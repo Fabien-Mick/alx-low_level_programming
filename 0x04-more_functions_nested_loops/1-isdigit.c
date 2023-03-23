b201e6e542fa314532d99e60cdf8324b5c096ba3
Fabien-Mick <fabien.mick@gmail.com>
2023-03-23 11:38:58 +0000

    refine the code

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,17 +7,12 @@
  */
 int _isdigit(int c)
 {
-	char i = '0';
-	int isdigit = 0;
-
-	for (; i <= '9'; i++)
+	if (c >= '0' && c <= '9')
 	{
-		if (i == c)
-		{
-			isdigit = 1;
-			break;
-		}
+		return (1);
 	}
-
-	return (isdigit);
+	else
+	{
+		return (0);
+	}
 }
