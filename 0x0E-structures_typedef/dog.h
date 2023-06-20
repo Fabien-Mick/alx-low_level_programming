6e1bb99f605e7c6ba31fec41bbfb5ba03e98b469
Fabien-Mick <fabien.mick@gmail.com>
2023-06-20 12:16:32 +0000

    Initialise a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - Information about my dog.
+  * @name: a char for his name
+  * @age: a float that contains his age
+  * @owner: a char for the name of the owner
+  */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
